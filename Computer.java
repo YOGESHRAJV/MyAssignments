10a4f453c3e90c8d551624a19c662d69cfa470ca
YOGESHRAJV <110474556+YOGESHRAJV@users.noreply.github.com>
2022-08-16 22:42:21 +0530

    Add files via upload

---
+++ Computer.java
@@ -0,0 +1,12 @@
+package org.system;
+
+public class Computer{
+	
+		public void computerModel() {
+			System.out.println("computerModel");
+
+	}
+public void deviceType() {
+	System.out.println("deviceType");
+}
+}
