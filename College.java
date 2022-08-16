05f08e47319d3efa23891bdbcbae6f182f7dfecb
YOGESHRAJV <110474556+YOGESHRAJV@users.noreply.github.com>
2022-08-16 22:40:09 +0530

    Add files via upload

---
+++ College.java
@@ -0,0 +1,14 @@
+package org.college;
+
+public class College {
+	public void collegeName() {
+		System.out.println("CEG,GUINDY");
+	}
+	public void collegeCode() {
+		System.out.println("001");
+	
+	}
+	  public void collegeRank() {
+		  System.out.println("1");
+	  }
+}
